cca06665f27fb378580870c0ba2f187aa9832d78
mgarfer499 <115544026+mgarfer499@users.noreply.github.com>
2023-11-01 16:01:47 +0100

    limpieza de archivos y loading

--- app/page.js
+++ app/page.js
@@ -9,16 +9,19 @@ export default function Home() {
   const [api, setApi] = useState(null)
   const [selectedRoute, setSelectedRoute] = useState('')
   const [emptyDates, setEmptyDates] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   // Función para obtener datos del calendario a través de la API creada
   const fetchCalendarData = async (route) => {
     try {
+      setIsLoading(true) // Muestra el indicador de carga
+
       const response = await fetch(`/api/allData/${route}`)
+
       if (response.ok) {
         const data = await response.json()
         setApi(data)
 
-        // Filtrar fechas sin datos
         const emptyDates = Object.keys(data.data).filter(
           (date) => data.data[date].length === 0
         )
@@ -28,6 +31,8 @@ export default function Home() {
       }
     } catch (error) {
       console.error('Error al realizar la solicitud a la API', error)
+    } finally {
+      setIsLoading(false) // Oculta el indicador de carga
     }
   }
 
@@ -50,7 +55,83 @@ export default function Home() {
         selectedRoute={selectedRoute}
         handleRouteChange={handleRouteChange}
       />
-      {api !== null ? (
+      {isLoading ? (
+        <svg
+          className='m-auto mt-12'
+          width='120'
+          height='30'
+          viewBox='0 0 120 30'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='#00000'
+        >
+          <circle cx='15' cy='15' r='15'>
+            <animate
+              attributeName='r'
+              from='15'
+              to='15'
+              begin='0s'
+              dur='0.8s'
+              values='15;9;15'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+            <animate
+              attributeName='fill-opacity'
+              from='1'
+              to='1'
+              begin='0s'
+              dur='0.8s'
+              values='1;.5;1'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+          </circle>
+          <circle cx='60' cy='15' r='9' fill-opacity='0.3'>
+            <animate
+              attributeName='r'
+              from='9'
+              to='9'
+              begin='0s'
+              dur='0.8s'
+              values='9;15;9'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+            <animate
+              attributeName='fill-opacity'
+              from='0.5'
+              to='0.5'
+              begin='0s'
+              dur='0.8s'
+              values='.5;1;.5'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+          </circle>
+          <circle cx='105' cy='15' r='15'>
+            <animate
+              attributeName='r'
+              from='15'
+              to='15'
+              begin='0s'
+              dur='0.8s'
+              values='15;9;15'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+            <animate
+              attributeName='fill-opacity'
+              from='1'
+              to='1'
+              begin='0s'
+              dur='0.8s'
+              values='1;.5;1'
+              calcMode='linear'
+              repeatCount='indefinite'
+            />
+          </circle>
+        </svg>
+      ) : api !== null ? (
         <Calendar emptyDates={emptyDates} />
       ) : (
         <h3 className='text-4xl font-semibold text-[#004998] text-center'>
